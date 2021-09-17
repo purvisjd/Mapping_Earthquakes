808f8ce6d261f552125e499807ec1265dbd1ee58
purvisjd <85641017+purvisjd@users.noreply.github.com>
2021-09-17 11:41:58 -0400

    Add files via upload

--- Mapping_Single_Points/static/js/logic.js
+++ Mapping_Single_Points/static/js/logic.js
@@ -2,7 +2,7 @@
 console.log("working");
 
 // create the map object with a center and zoom level
-let map = L.map('mapid').setView([40.7, -94.5], 4);
+let map = L.map('mapid').setView([34.0522, -118.2437], 14);
 
 //alternative method for using setView()
 //let map = L.map("mapid", {
@@ -12,8 +12,16 @@ let map = L.map('mapid').setView([40.7, -94.5], 4);
 //  zoom: 4
 // });
 
+//  add a marker to the map for Los Angeles, California
+//let marker = L.marker([34.0522, -118.2437]).addTo(map);
+L.circleMarker([34.0522, -118.2437], {
+    radius: 300,
+    color: "black",
+    fillColor: "#ffffa1"
+}).addTo(map);
+
 //create tile layer that will be background of the map
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     accessToken: API_KEY
